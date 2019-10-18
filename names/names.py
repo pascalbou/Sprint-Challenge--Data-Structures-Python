728cc1de052ce4b42ad20b85a29ef4933c9d2ba7
krikaz <pascal.bou@protonmail.com>
2019-10-18 11:50:55 +0200

    finished names, runtime 1/10 second

--- names/names.py
+++ names/names.py
@@ -18,6 +18,24 @@ duplicates = []
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
+# Final solution
+# create 26 lists for each of letter in alphabet
+# loop through names_1 and append name according to its first letter
+# loop through names_2 and only search in the array with the same first letter
+# compute time is around 1/10th of a second
+
+all_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+all_names1 = {letter: [] for letter in all_letters}
+for name_1 in names_1:
+    first_letter = name_1[0].lower()
+    all_names1[first_letter].append(name_1)
+# print(all_names1)
+
+for name_2 in names_2:
+    first_letter = name_2[0].lower()
+    if name_2 in all_names1[name_2[0].lower()]:
+        duplicates.append(name_2)
+
 # first solution
 # try hash table
 # each array contains 26 letters
@@ -48,6 +66,14 @@ duplicates = []
 #         if name_1[i-1] in all_names
 
 # print(all_names)
+
+# third solution
+# use hash function
+    # return int according to first letter of name
+    # example A returns 0, B returns 1
+    # do the same for last name
+# create 26 lists for each letter
+# loop through names2, should be faster than looping through whole names1 list
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
